caa6313d7e0d46a342e30351bef44de243d4dfb3
Max1Manley <37525559+Max1Manley@users.noreply.github.com>
2018-12-03 13:07:12 -0600

    color coding added, commented out graph

--- Compair/script.js
+++ Compair/script.js
@@ -12,9 +12,112 @@ let c3temp = document.getElementById('c3temp');
 let c3humid = document.getElementById('c3humid');
 let c3wind = document.getElementById('c3wind');
 
+let canvas = document.getElementById('canvas');
+
 let api = 'https://api.openweathermap.org/data/2.5/weather?q=';
 let apiKeyAndUnits = '&APPID=727c2f55c3d9cc48f85080101f3b4ef0&units=imperial';
 
+
+
+const c1weatherAsk = () => {
+
+	//Setting up API URL
+	let city = c1input.value;
+	const url = api + city + apiKeyAndUnits;
+
+	//Fetching and Displaying OpenWeather Data
+	fetch(url)
+	.then((res) => res.json())
+	.then(function(data){
+		console.log(data);
+		c1temp.innerHTML = Math.round(data.main.temp) + '°';
+		c1humid.innerHTML = Math.round(data.main.humidity) + '%';
+		c1wind.innerHTML = Math.round(data.wind.speed) + 'mph';
+
+		calculate();
+		classToggle();
+
+	})
+
+	//Alert Error
+	.catch(function(error){
+		console.log(JSON.stringify(error))
+		alert('City not found.');
+	})
+
+}
+
+const c3weatherAsk = () => {
+
+	//Setting up API URL
+	let city = c3input.value;
+	const url = api + city + apiKeyAndUnits;
+
+	//Fetching an Displaying OpenWeather Data
+	fetch(url)
+	.then((res) => res.json())
+	.then(function(data){
+		console.log(data);
+		c3temp.innerHTML = Math.round(data.main.temp) + '°';
+		c3humid.innerHTML = Math.round(data.main.humidity) + '%';
+		c3wind.innerHTML = Math.round(data.wind.speed) + 'mph';
+
+		calculate();
+		classToggle();
+
+	})
+
+	//Alert Error
+	.catch(function(error){
+		console.log(JSON.stringify(error))
+		alert('City not found.');
+	})
+
+}
+
+const classToggle = () => {
+
+	//Clearing class names
+	c2temp.className = "";
+	c2humid.className = "";
+	c2wind.className = "";
+
+	//Comparing temps and changing class
+	if (c1input.value != '' && c3input.value != ''){
+		if (Math.abs(parseFloat(c1temp.innerHTML)) > Math.abs(parseFloat(c3temp.innerHTML))) {
+			c2temp.classList.toggle("blue");
+		} else if (Math.abs(parseFloat(c1temp.innerHTML)) < Math.abs(parseFloat(c3temp.innerHTML))) {
+			c2temp.classList.toggle("orange");
+		} else {
+			c2temp.classList.toggle("black");
+		}
+	}
+
+	//Comparing humidity
+	if (c1input.value != '' && c3input.value != ''){
+		if (Math.abs(parseFloat(c1humid.innerHTML)) > Math.abs(parseFloat(c3humid.innerHTML))) {
+			c2humid.classList.toggle("blue");
+		} else if (Math.abs(parseFloat(c1humid.innerHTML)) < Math.abs(parseFloat(c3humid.innerHTML))) {
+			c2humid.classList.toggle("orange");
+		} else {
+			c2humid.classList.toggle("black");
+		}
+	}
+
+	//Comparing wind
+	if (c1input.value != '' && c3input.value != ''){
+		if (Math.abs(parseFloat(c1wind.innerHTML)) > Math.abs(parseFloat(c3wind.innerHTML))) {
+			c2wind.classList.toggle("blue");
+		} else if (Math.abs(parseFloat(c1wind.innerHTML)) < Math.abs(parseFloat(c3wind.innerHTML))) {
+			c2wind.classList.toggle("orange");
+		} else {
+			c2wind.classList.toggle("black");
+		}
+	}
+
+}
+
+//Calculates and displays difference between cities
 const calculate = () => {
 	if (c1input.value != '' && c3input.value != ''){
 		c2temp.innerHTML = Math.abs(parseFloat(c1temp.innerHTML) - parseFloat(c3temp.innerHTML)) + '°';
@@ -23,49 +126,11 @@ const calculate = () => {
 	}
 }
 
-const c1weatherAsk = () => {
-	let city = c1input.value;
-	const url = api + city + apiKeyAndUnits;
 
-	//Fetching OpenWeather Data
-	fetch(url)
-	.then((res) => res.json())
-	.then(function(data){
-		console.log(data);
-		c1temp.innerHTML = Math.ceil(data.main.temp) + '°';
-		c1humid.innerHTML = Math.ceil(data.main.humidity) + '%';
-		c1wind.innerHTML = Math.ceil(data.wind.speed) + 'mph';
 
-		calculate();
+//Making graph
+// let av = -100;
+// var c = canvas.getContext('2d');
 
-	})
-
-	.catch(function(error){
-		console.log(JSON.stringify(error));
-	})
-
-}
-
-const c3weatherAsk = () => {
-	let city = c3input.value;
-	const url = api + city + apiKeyAndUnits;
-
-	//Fetching OpenWeather Data
-	fetch(url)
-	.then((res) => res.json())
-	.then(function(data){
-		console.log(data);
-		c3temp.innerHTML = Math.ceil(data.main.temp) + '°';
-		c3humid.innerHTML = Math.ceil(data.main.humidity) + '%';
-		c3wind.innerHTML = Math.ceil(data.wind.speed) + 'mph';
-
-		calculate();
-
-	})
-
-	.catch(function(error){
-		console.log(JSON.stringify(error))
-		alert('City not found.');
-	})
-
-}
+// c.fillStyle = 'rgb(0, 255, 255)';
+// c.fillRect(50, 610, 75, av);
